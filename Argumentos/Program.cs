ec4569a1a80af74df612c2bfaa23433a8b96b1d4
Miguel Almeida <a22103547@alunos.ulht.pt>
2022-03-03 14:48:27 +0000

    Comment 01

---
+++ Argumentos/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Argumentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			foreach(string s in args){
+				Console.WriteLine(" -------  ");
+				Console.WriteLine(s);
+			}
+            //Console.WriteLine("Hello World!");
+			//Console.WriteLine("Bye World!!");
+        }
+    }
+}
